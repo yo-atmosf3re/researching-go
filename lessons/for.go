9d7a72dcf52ccf9b2196f0d23bcac8bb737e23ce
Alexandr Shishov <ernesto.chrome@mail.ru>
2026-04-25 19:29:57 +0300

    feat: for;

--- lessons/for.go
+++ lessons/for.go
@@ -1,6 +1,10 @@
 package lessons
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func pt(value ...any) (n int, err error) {
 	return fmt.Println(value...)
@@ -12,4 +16,36 @@ func ForExample() {
 		pt("first", index)
 	}
 	pt(index)
+}
+
+func WithRand() {
+	randomValue := rand.Intn(10)
+
+	for i := 0; i <= randomValue; i++ {
+		pt("second", i)
+	}
+}
+
+func SleepExample() {
+	for i := 0; i <= 5; i++ {
+		pt("third", i)
+		time.Sleep(1 * time.Minute)
+	}
+}
+
+func Infinity() {
+	score := 0
+	randValue := rand.Intn(20)
+	for {
+		pt("before: ", score)
+		pt("rand value: ", randValue)
+		if randValue == score {
+			pt("end")
+			break
+		}
+		score++
+		pt("after: ", score)
+		time.Sleep(500 * time.Millisecond)
+	}
+	pt("total: ", score)
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	lessons.ForExample()
+	lessons.Infinity()
 }
