cf51119561a38e15e94993210593fac6cb4ab5d2
Alexandr Shishov <ernesto.chrome@mail.ru>
2026-05-04 14:51:45 +0300

    feat: studying select is continue;

--- lessons/select.go
+++ lessons/select.go
@@ -2,7 +2,6 @@ package lessons
 
 import (
 	"researching-go/pkg/logger"
-	"strconv"
 	"time"
 )
 
@@ -11,34 +10,69 @@ func selectExample1() {
 	strCh := make(chan string)
 
 	go func() {
-		i := 1
-		for {
-			intCh <- i
-			i++
-			time.Sleep(1 * time.Second)
-		}
+		time.Sleep(40 * time.Millisecond)
+		intCh <- 1
+
 	}()
 
 	go func() {
-		i := 1
+		time.Sleep(100 * time.Millisecond)
+		strCh <- "str"
+
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+
+	select {
+	case number := <-intCh:
+		logger.Ptc("intCh: ", number)
+	case str := <-strCh:
+		logger.Ptc("strCh: ", str)
+	default: // this code works if case-channels is not ready. if case-channel condition is ready, that default don't work
+		logger.Ptc("channel is not ready")
+	}
+
+}
+
+type message struct {
+	author string
+	text   string
+}
+
+func selectExample2() {
+	messageChannel1 := make(chan message)
+	messageChannel2 := make(chan message)
+
+	go func() {
 		for {
-			strCh <- "str" + strconv.Itoa(i)
-			i++
+			messageChannel1 <- message{
+				author: "Alex",
+				text:   "hello",
+			}
+			time.Sleep(10 * time.Second)
+		}
+
+	}()
+	go func() {
+		for {
+			messageChannel2 <- message{
+				author: "Dave",
+				text:   "hello",
+			}
 			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 
 	for {
 		select {
-		case number := <-intCh:
-			logger.Ptc("intCh: ", number)
-		case str := <-strCh:
-			logger.Ptc("strCh: ", str)
+		case msg1 := <-messageChannel1:
+			logger.Ptc("i got message from: ", msg1.author, "text of message", msg1.text)
+		case msg2 := <-messageChannel2:
+			logger.Ptc("i got message from: ", msg2.author, "text of message", msg2.text)
 		}
 	}
-
 }
 
 func SelectExample() {
-	selectExample1()
+	selectExample2()
 }
