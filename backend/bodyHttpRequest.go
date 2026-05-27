37809f32c19056797b0d59678f8e42522439900d
Alexandr Shishov <ernesto.chrome@mail.ru>
2026-05-27 17:27:16 +0300

    feat: http body №2;

--- backend/bodyHttpRequest.go
+++ backend/bodyHttpRequest.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 	"researching-go/pkg/logger"
 	"strconv"
+	"sync"
 	"sync/atomic"
+	"time"
 )
 
-var money = atomic.Int64{} // handler it is function inside goroutine, so atomic should be used
+var money = atomic.Int64{} // handler it is function inside goroutine, so atomic should be used + money it is variable which using in different http handle function, mtx is expected
 var bank = atomic.Int64{}
+var mtx sync.Mutex // declare mutex can outside functions globally, e.g. near other variables, because we can't receive mtx as argument in http handle function
 
 func handlePayment(w http.ResponseWriter, r *http.Request) {
 	rBody, err := io.ReadAll(r.Body)
@@ -26,12 +29,15 @@ func handlePayment(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	mtx.Lock()
 	if money.Load()-int64(paymentAmount) >= 0 {
+		time.Sleep(3 * time.Second)
 		money.Add(int64(-paymentAmount))
 	} else {
-		logger.Ptc("is not enough money")
+		logger.Ptc("payment: is not enough money")
 	}
 	logger.Ptc("money", money.Load())
+	mtx.Unlock()
 
 }
 
@@ -48,17 +54,20 @@ func handleSaveBalance(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	mtx.Lock()
 	if money.Load()-int64(saveAmount) >= 0 {
+		time.Sleep(3 * time.Second)
 		money.Add(int64(-saveAmount)) // convert int to int64 for atomic
 		bank.Add(int64(saveAmount))
 		logger.Ptc("money :", money.Load(), "bank :", bank.Load())
 	} else {
-		logger.Ptc("insufficient funds")
+		logger.Ptc("save balance: insufficient funds")
 	}
+	mtx.Unlock()
 }
 
 func run() {
-	money.Add(1000)
+	money.Add(50)
 	http.HandleFunc("/pay", handlePayment)
 	http.HandleFunc("/save", handleSaveBalance)
 	logger.Ptc("starting server")
