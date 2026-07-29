64d4318d46baab3cdf0ad241997f51a7b7aa2903
Alexandr Shishov <ernesto.chrome@mail.ru>
2026-07-29 22:51:16 +0300

    feat: preparing rest api 2;

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module researching-go
 
 go 1.26.2
 
-require github.com/k0kubun/pp/v3 v3.5.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/k0kubun/pp/v3 v3.5.1
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
---
+++ rest_api/http/server.go
@@ -0,0 +1,30 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type HTTPServer struct {
+	httpHandlers *HTTPHandlers
+}
+
+func NewHTTPHServer(httpHandler *HTTPHandlers) *HTTPServer {
+	return &HTTPServer{
+		httpHandlers: httpHandler,
+	}
+}
+
+func (s *HTTPServer) StartServer() error {
+	router := mux.NewRouter()
+
+	router.Path("/tasks").Methods("POST").HandlerFunc(s.httpHandlers.HandleCreateTask)
+	router.Path("/tasks/{title}").Methods("GET").HandlerFunc(s.httpHandlers.HandleGetTask)
+	router.Path("/tasks").Methods("GET").HandlerFunc(s.httpHandlers.HandleGetAllTasks)
+	router.Path("/tasks").Methods("GET").Queries("completed", "true").HandlerFunc(s.httpHandlers.HandleGetAllUncompletedTasks)
+	router.Path("/tasks/{title}").Methods("PATCH").HandlerFunc(s.httpHandlers.HandleCompleteTask)
+	router.Path("/tasks/{title}").Methods("DELETE").HandlerFunc(s.httpHandlers.HandleDeleteTask)
+
+	return http.ListenAndServe(":9091", router)
+}
